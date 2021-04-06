3b9875db0c12dbba842ae6679b66945f2634be15
Edward <edwardcahuahuayta@hotmail.com>
2021-04-06 18:55:07 -0300

    segundo commit

--- verAfazer.vb
+++ verAfazer.vb
@@ -4,7 +4,7 @@ Public Class verAfazer
     Class Afazer
         Dim panel As New Panel()
         Dim lbl_dataCadastro As New Label()
-        Dim dtp_dataCadastro As New DateTimePicker()
+        Dim lbl_dataCadastroValor As New Label()
         Dim lbl_titulo As New Label()
         Dim txt_titulo As New TextBox()
         Dim lbl_prazo As New Label()
@@ -23,7 +23,7 @@ Public Class verAfazer
         Friend Sub New(ByVal frm As verAfazer, ByVal _dataCadastro As DateTime, ByVal _titulo As String, ByVal _prazo As DateTime, ByVal _estado As Integer, ByVal _detalhes As String)
             'adicionando controles no panel
             panel.Controls.Add(lbl_dataCadastro)
-            panel.Controls.Add(dtp_dataCadastro)
+            panel.Controls.Add(lbl_dataCadastroValor)
             panel.Controls.Add(lbl_titulo)
             panel.Controls.Add(txt_titulo)
             panel.Controls.Add(lbl_prazo)
@@ -37,7 +37,7 @@ Public Class verAfazer
 
             'colocando fonte 12 para todos os itens
             lbl_dataCadastro.Font = fonte
-            dtp_dataCadastro.Font = fonte
+            lbl_dataCadastroValor.Font = fonte
             lbl_titulo.Font = fonte
             txt_titulo.Font = fonte
             lbl_prazo.Font = fonte
@@ -47,7 +47,7 @@ Public Class verAfazer
             btn_modificar.Font = fonte
             btn_addnotas.Font = fonte
             lbl_detalhes.Font = fonte
-            txt_detalhes.Font = fonte
+            txt_detalhes.Font = New Font("Microsoft Sans Serif", 8)
 
             'labels
             lbl_dataCadastro.Text = "Data do Cadastro:"
@@ -55,9 +55,11 @@ Public Class verAfazer
             lbl_prazo.Text = "Prazo"
             lbl_estado.Text = "Estado"
             lbl_detalhes.Text = "Detalhes"
+            btn_modificar.Text = "Editar"
+            btn_addnotas.Text = "add. notas"
 
             'conteudo dos controles
-            dtp_dataCadastro.Value = _dataCadastro
+            lbl_dataCadastroValor.Text = _dataCadastro
             txt_titulo.Text = _titulo
             dtp_prazo.Value = _prazo
             cbx_estado.Items.AddRange({"Não feito", "Feito", "Em andamento"})
@@ -65,31 +67,53 @@ Public Class verAfazer
             txt_detalhes.Text = _detalhes
 
             'tamanho dos controles
-            panel.Size = New Size(700, 200)
-            lbl_dataCadastro = New Si
+            panel.Size = New Size(660, 170)
+            lbl_dataCadastro.Size = New Size(140, 20)
+            lbl_dataCadastroValor.Size = New Size(110, 20)
+            lbl_titulo.Size = New Size(260, 20)
+            txt_titulo.Size = New Size(260, 26)
+            lbl_prazo.Size = New Size(110, 20)
+            dtp_prazo.Size = New Size(110, 26)
+            lbl_estado.Size = New Size(140, 20)
+            cbx_estado.Size = New Size(140, 20)
+            btn_modificar.Size = New Size(100, 30)
+            btn_addnotas.Size = New Size(100, 30)
+            lbl_detalhes.Size = New Size(640, 20)
+            'lbl_detalhes.BackColor = New Color().FromArgb(255, 215, 0, 0)
+            txt_detalhes.Size = New Size(640, 60)
+
+
+
 
             'posição dos controles
-            panel.Location = New Point(10, 10)
+            panel.Location = New Point(40, 50)
             panelx = panel.Location.X
             panely = panel.Location.Y
 
-            lbl_dataCadastro.Location = New Point(panel.Width / 2 - ((lbl_dataCadastro.Width + dtp_dataCadastro.Width) / 2), panely + 20)
-            dtp_dataCadastro.Location = New Point(panel.Width / 2 + ((lbl_dataCadastro.Width + dtp_dataCadastro.Width) / 2), panely + 20)
-            lbl_titulo.Location = New Point(panelx + 95, panely + 60)
-            txt_titulo.Location = New Point(panelx + 20, panely + 90)
-            lbl_prazo.Location = New Point(panelx + 295, panely + 60)
-            dtp_prazo.Location = New Point(panelx + 260, panely + 90)
-            lbl_estado.Location = New Point(panelx + 440, panely + 60)
-            cbx_estado.Location = New Point(panelx + 410, panely + 90)
-            btn_modificar.Location = New Point(panelx + 540, panely + 90)
-            btn_addnotas.Location = New Point(panelx + 630, panely + 90)
-            lbl_detalhes.Location = New Point(panel.Width / 2 - (lbl_detalhes.Width / 2), panely + 135)
-            txt_detalhes.Location = New Point(panel.Width / 2 - (txt_detalhes.Width / 2), panely + 150)
+            lbl_dataCadastro.Location = New Point(panel.Width / 2 - (lbl_dataCadastro.Width), 2)
+            lbl_dataCadastroValor.Location = New Point(panel.Width / 2, 2)
+            lbl_titulo.Location = New Point(10, 26)
+            txt_titulo.Location = New Point(10, 48)
+            lbl_prazo.Location = New Point(280, 26)
+            dtp_prazo.Location = New Point(280, 48)
+            lbl_estado.Location = New Point(400, 26)
+            cbx_estado.Location = New Point(400, 48)
+            btn_addnotas.Location = New Point(550, 16)
+            btn_modificar.Location = New Point(550, 47)
+            lbl_detalhes.Location = New Point(10, 80)
+            txt_detalhes.Location = New Point(10, 100)
 
             'configurações especificas
-            dtp_dataCadastro.Format = DateTimePickerFormat.Short
+            panel.BorderStyle = BorderStyle.FixedSingle
             dtp_prazo.Format = DateTimePickerFormat.Short
-
+            lbl_titulo.TextAlign = ContentAlignment.MiddleCenter
+            lbl_prazo.TextAlign = ContentAlignment.MiddleCenter
+            lbl_detalhes.TextAlign = ContentAlignment.MiddleCenter
+            txt_detalhes.Multiline = True
+            txt_titulo.ReadOnly = True
+            txt_detalhes.ReadOnly = True
+            dtp_prazo.Enabled = False
+            cbx_estado.Enabled = False
 
 
             frm.Controls.Add(panel)
@@ -107,10 +131,11 @@ Public Class verAfazer
     Private Sub verAfazer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Create a Connection object.
         conexao = New SqlConnection("Initial Catalog=auxiliar;" & "Data Source=localhost;Integrated Security=SSPI;")
+        'conexao = New SqlConnection("Initial Catalog=auxiliar;" & "Data Source=VM-CPD3\DBTESTE;Integrated Security=SSPI;")
 
         'Create a Command object.
         consulta = conexao.CreateCommand
-        consulta.CommandText = "select afazer_dataatual,afazer_titulo,afazer_detalhes ,afazer_prazo,afazer_status from tb_afazer ORDER BY afazer_id desc OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY"
+        consulta.CommandText = "select afazer_dataatual,afazer_titulo,afazer_detalhes ,afazer_prazo,afazer_status from tb_afazer ORDER BY afazer_id desc OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY"
 
         'Open the connection.
         conexao.Open()
@@ -121,27 +146,27 @@ Public Class verAfazer
         Dim y As Integer
         y = 10
 
-        'Do While myReader.Read()
+        Do While myReader.Read()
 
-        '    Dim dataCadastro As DateTime
-        '    Dim titulo As String
-        '    Dim detalhes As String
-        '    Dim prazo As DateTime
-        '    Dim estado As String
-
-
-
-        '    dataCadastro = myReader.GetDateTime(0)
-        '    titulo = myReader.GetString(1)
-        '    detalhes = myReader.GetString(2)
-        '    prazo = myReader.GetDateTime(3)
-        '    estado = myReader.GetByte(4)
+            Dim dataCadastro As DateTime
+            Dim titulo As String
+            Dim detalhes As String
+            Dim prazo As DateTime
+            Dim estado As String
 
 
 
-        'Loop
+            dataCadastro = myReader.GetDateTime(0)
+            titulo = myReader.GetString(1)
+            detalhes = myReader.GetString(2)
+            prazo = myReader.GetDateTime(3)
+            estado = myReader.GetByte(4)
 
-        Dim a1 As New Afazer(Me, "10/03/2021", "titulo", "11/03/2021", 1, "detalhes")
+            Dim a1 As New Afazer(Me, dataCadastro, titulo, prazo, estado, detalhes)
+
+        Loop
+
+
 
         'MsgBox(estado)
 
