38c77641082eb645f9e1ad1310e523ad4c011f68
Edward <edwardcahuahuayta@hotmail.com>
2021-07-19 16:28:28 -0300

    apagando tudo

--- conexao.vb
+++
@@ -1,4 +0,0 @@
-﻿Public Class globalConexao
-    Public Shared initial As String = "Initial Catalog=controle;"
-    Public Shared data As String = "Data Source=localhost;Integrated Security=SSPI;"
-End Class
