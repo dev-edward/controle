e41a802bbf14edf5009d9f2a7784726ecad8aaf7
Edward <edwardcahuahuayta@hotmail.com>
2021-09-03 16:35:57 -0300

    03/09/2021

--- estadoAfazer.Designer.vb
+++ estadoAfazer.Designer.vb
@@ -27,6 +27,9 @@ Partial Class estadoAfazer
         Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.RadioButton3 = New System.Windows.Forms.RadioButton()
         Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.btn_cancelar = New System.Windows.Forms.Button()
+        Me.btn_alterar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'RadioButton1
@@ -34,7 +37,7 @@ Partial Class estadoAfazer
         Me.RadioButton1.AutoSize = True
         Me.RadioButton1.Image = CType(resources.GetObject("RadioButton1.Image"), System.Drawing.Image)
         Me.RadioButton1.ImageAlign = System.Drawing.ContentAlignment.BottomLeft
-        Me.RadioButton1.Location = New System.Drawing.Point(12, 12)
+        Me.RadioButton1.Location = New System.Drawing.Point(20, 38)
         Me.RadioButton1.Name = "RadioButton1"
         Me.RadioButton1.Size = New System.Drawing.Size(95, 83)
         Me.RadioButton1.TabIndex = 0
@@ -49,7 +52,7 @@ Partial Class estadoAfazer
         Me.RadioButton2.AutoSize = True
         Me.RadioButton2.Image = CType(resources.GetObject("RadioButton2.Image"), System.Drawing.Image)
         Me.RadioButton2.ImageAlign = System.Drawing.ContentAlignment.BottomLeft
-        Me.RadioButton2.Location = New System.Drawing.Point(115, 12)
+        Me.RadioButton2.Location = New System.Drawing.Point(120, 38)
         Me.RadioButton2.Name = "RadioButton2"
         Me.RadioButton2.Size = New System.Drawing.Size(106, 83)
         Me.RadioButton2.TabIndex = 1
@@ -64,7 +67,7 @@ Partial Class estadoAfazer
         Me.RadioButton3.AutoSize = True
         Me.RadioButton3.Image = CType(resources.GetObject("RadioButton3.Image"), System.Drawing.Image)
         Me.RadioButton3.ImageAlign = System.Drawing.ContentAlignment.BottomLeft
-        Me.RadioButton3.Location = New System.Drawing.Point(218, 12)
+        Me.RadioButton3.Location = New System.Drawing.Point(220, 38)
         Me.RadioButton3.Name = "RadioButton3"
         Me.RadioButton3.Size = New System.Drawing.Size(78, 83)
         Me.RadioButton3.TabIndex = 2
@@ -79,7 +82,7 @@ Partial Class estadoAfazer
         Me.RadioButton4.AutoSize = True
         Me.RadioButton4.Image = CType(resources.GetObject("RadioButton4.Image"), System.Drawing.Image)
         Me.RadioButton4.ImageAlign = System.Drawing.ContentAlignment.BottomLeft
-        Me.RadioButton4.Location = New System.Drawing.Point(321, 12)
+        Me.RadioButton4.Location = New System.Drawing.Point(320, 38)
         Me.RadioButton4.Name = "RadioButton4"
         Me.RadioButton4.Size = New System.Drawing.Size(84, 83)
         Me.RadioButton4.TabIndex = 3
@@ -89,11 +92,42 @@ Partial Class estadoAfazer
         Me.RadioButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
         Me.RadioButton4.UseVisualStyleBackColor = True
         '
+        'btn_cancelar
+        '
+        Me.btn_cancelar.Location = New System.Drawing.Point(110, 146)
+        Me.btn_cancelar.Name = "btn_cancelar"
+        Me.btn_cancelar.Size = New System.Drawing.Size(90, 30)
+        Me.btn_cancelar.TabIndex = 4
+        Me.btn_cancelar.Text = "Cancelar"
+        Me.btn_cancelar.UseVisualStyleBackColor = True
+        '
+        'btn_alterar
+        '
+        Me.btn_alterar.Location = New System.Drawing.Point(240, 146)
+        Me.btn_alterar.Name = "btn_alterar"
+        Me.btn_alterar.Size = New System.Drawing.Size(90, 30)
+        Me.btn_alterar.TabIndex = 5
+        Me.btn_alterar.Text = "Alterar"
+        Me.btn_alterar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(98, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(200, 26)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Alterar status do afazer para:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'estadoAfazer
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(442, 185)
+        Me.ClientSize = New System.Drawing.Size(424, 191)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_alterar)
+        Me.Controls.Add(Me.btn_cancelar)
         Me.Controls.Add(Me.RadioButton4)
         Me.Controls.Add(Me.RadioButton3)
         Me.Controls.Add(Me.RadioButton2)
@@ -109,4 +143,7 @@ Partial Class estadoAfazer
     Friend WithEvents RadioButton2 As RadioButton
     Friend WithEvents RadioButton3 As RadioButton
     Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents btn_cancelar As Button
+    Friend WithEvents btn_alterar As Button
+    Friend WithEvents Label1 As Label
 End Class
--- estadoAfazer.vb
+++ estadoAfazer.vb
@@ -1,3 +1,58 @@
-﻿Public Class estadoAfazer
+﻿Imports System.Data.SqlClient
+Public Class estadoAfazer
+    Private conexao As SqlConnection
+    Private consulta As SqlCommand
+    Private myReader As SqlDataReader
 
+    Dim pk As Integer
+    Private Sub New(ByVal _pk As Integer)
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+        pk = _pk
+
+    End Sub
+    Private Sub estadoAfazer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'AddHandler btn_addnotas.Click, AddressOf btn_addnotas_Click
+    End Sub
+
+    Private Sub btn_cancelar_Click(sender As Object, e As EventArgs) Handles btn_cancelar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btn_alterar_Click(sender As Object, e As EventArgs) Handles btn_alterar.Click
+        pk = 1
+
+        Dim cbx_checked = Me.Controls
+        .OfType(Of RadioButton)
+       .FirstOrDefault(Function(r) r.Checked = True)
+
+        Try
+            conexao = New SqlConnection(globalConexao.initial & globalConexao.data)
+
+            consulta = conexao.CreateCommand
+            consulta.CommandText = "UPDATE tb_afazer SET 
+                                afazer_dtalteracao = GETDATE(),
+                                afazer_useralteracao = @useralteracao,
+                                afazer_status = @status,
+                                WHERE afazer_id = @id"
+
+            consulta.Parameters.AddWithValue("@useralteracao", usuario.usuario_id)
+            consulta.Parameters.AddWithValue("@status", cbx_estado.SelectedIndex + 1)
+            consulta.Parameters.AddWithValue("@id", pk)
+
+            conexao.Open()
+
+            consulta.ExecuteNonQuery()
+
+
+        Catch ex As Exception
+            MessageBox.Show("Erro ao atualizar: " & ex.Message, "Insert Records")
+        Finally
+            conexao.Close()
+        End Try
+    End Sub
 End Class
